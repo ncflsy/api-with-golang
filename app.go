9481fead1d14c1597b3bbbd132cdd3acd65dd9fa
nico flassy <flassynico@gmail.com>
2023-10-01 17:35:07 +0700

    first commit

---
+++ app.go
@@ -0,0 +1,40 @@
+package main
+
+import {
+	"net/http"
+	"github.com/gin-gonic/gin"
+}
+
+type todo struct{
+	ID			string	`json:"id"`
+	Item		string	`json:title`
+	Completed	bool	`json:completed`
+}
+
+var todos = []todo{
+	{
+		ID:			"1",
+		Item:		"Clean Room",
+		Completed:	false,
+	},
+	{
+		ID:			"2",
+		Item:		"Read Book",
+		Completed:	false,
+	},
+	{
+		ID:			"3",
+		Item:		"Record Video",
+		Completed:	false,
+	}
+}
+
+func getTodos(context *gin.Context){
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func main(){
+	router :=gin .Default()
+	router.GET("/todos", getTodos)
+	router.Run("localhost:9090")
+}
