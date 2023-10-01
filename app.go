171c49fcac570f9682e31fe399d390dbc56f54c8
nico flassy <flassynico@gmail.com>
2023-10-01 17:41:51 +0700

    inisialize modul go

--- app.go
+++ app.go
@@ -1,40 +1,41 @@
 package main
 
-import {
+import (
 	"net/http"
-	"github.com/gin-gonic/gin"
-}
 
-type todo struct{
-	ID			string	`json:"id"`
-	Item		string	`json:title`
-	Completed	bool	`json:completed`
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"item"` // Fixed the typo here
+	Completed bool   `json:"completed"`
 }
 
 var todos = []todo{
 	{
-		ID:			"1",
-		Item:		"Clean Room",
-		Completed:	false,
+		ID:        "1",
+		Item:      "Clean Room",
+		Completed: false,
 	},
 	{
-		ID:			"2",
-		Item:		"Read Book",
-		Completed:	false,
+		ID:        "2",
+		Item:      "Read Book",
+		Completed: false,
 	},
 	{
-		ID:			"3",
-		Item:		"Record Video",
-		Completed:	false,
-	}
+		ID:        "3",
+		Item:      "Record Video",
+		Completed: false,
+	},
 }
 
-func getTodos(context *gin.Context){
+func getTodos(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos)
 }
 
-func main(){
-	router :=gin .Default()
+func main() {
+	router := gin.Default()
 	router.GET("/todos", getTodos)
 	router.Run("localhost:9090")
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module api-with-golang
+
+go 1.21.1
